52dfa3b6f0ca41cc6a776b775f7918d479d1e1cc
PieroMiranda1 <pieroms25@gmail.com>
2024-09-21 11:05:03 -0500

    feat: added html sctructure for sidebar

--- src/app/public/components/sidebar-navigator/sidebar-navigator.component.ts
+++ src/app/public/components/sidebar-navigator/sidebar-navigator.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import {MatSidenav, MatSidenavContainer} from "@angular/material/sidenav";
+import {MatListItem, MatNavList} from "@angular/material/list";
+import {MatIcon} from "@angular/material/icon";
+import {MatButton} from "@angular/material/button";
+import {RouterLink, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-sidebar-navigator',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSidenavContainer,
+    MatSidenav,
+    MatNavList,
+    MatListItem,
+    MatIcon,
+    MatButton,
+    RouterLink,
+    RouterOutlet
+  ],
   templateUrl: './sidebar-navigator.component.html',
   styleUrl: './sidebar-navigator.component.css'
 })
