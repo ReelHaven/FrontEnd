1020eb3c517369497a09e2b7c45628cd67922ac9
PieroMiranda1 <pieroms25@gmail.com>
2024-09-21 10:58:47 -0500

    feat: added sidebar component

---
+++ src/app/public/components/sidebar-navigator/sidebar-navigator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-navigator',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar-navigator.component.html',
+  styleUrl: './sidebar-navigator.component.css'
+})
+export class SidebarNavigatorComponent {
+
+}
