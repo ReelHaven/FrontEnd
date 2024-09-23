58c93dffde8fc90376d879e9e1faef2c7678cf3b
PieroMiranda1 <pieroms25@gmail.com>
2024-09-23 18:36:34 -0500

    feat: added toolbar component

--- src/app/public/components/toolbar-login/toolbar-login.component.ts
+++ src/app/public/components/toolbar-login/toolbar-login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-toolbar-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbar
+  ],
   templateUrl: './toolbar-login.component.html',
   styleUrl: './toolbar-login.component.css'
 })
