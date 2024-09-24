e8671c9a5073e7e14261bdec3211a13a7de4bf5a
PieroMiranda1 <pieroms25@gmail.com>
2024-09-23 19:28:38 -0500

    feat: added home page

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SidebarNavigatorComponent} from "../../components/sidebar-navigator/sidebar-navigator.component";
+import {MoodService} from "../../../users/services/mood.service";
+import {RecommendationContentComponent} from "../../components/recommendation-content/recommendation-content.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    SidebarNavigatorComponent,
+    RecommendationContentComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  user: any;
+
+  constructor() {
+    this.user= localStorage.getItem('currentUser');
+
+    if(this.user){
+      this.user = JSON.parse(this.user);
+    }
+    console.log('user name: ', this.user.name);
+  }
+
+  /*
+  ngOnInit() {
+    const moodValues = JSON.parse(localStorage.getItem('moodValues')!);
+
+    this.moodService.getFilteredContent(moodValues)
+      .subscribe(result => {
+        console.log("Content for feeling match:", result.feelingMatch);
+        console.log("Content for objective match:", result.objectiveMatch);
+        console.log("Content for category match:", result.categoryMatch);
+      })
+  }
+
+   */
 }
