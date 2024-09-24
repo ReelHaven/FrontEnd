ecbbd302c7549f16d509bee728a3d07d8d443dae
U202213553 <u202213553@upc.edu.pe>
2024-09-23 21:58:58 -0500

    Update forum-list.component.ts

--- src/app/forums/pages/forum-list/forum-list.component.ts
+++ src/app/forums/pages/forum-list/forum-list.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {ListRoomsComponent} from "../../components/list-rooms/list-rooms.component";
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {CreateRoomDialogComponent} from "../../components/create-room-dialog/create-room-dialog.component";
 
 @Component({
-  selector: 'app-forum-list',
+  selector: 'app-rooms-view',
   standalone: true,
-  imports: [],
-  templateUrl: './forum-list.component.html',
-  styleUrl: './forum-list.component.css'
+  imports: [
+    ListRoomsComponent,MatDialogModule
+  ],
+  templateUrl: './rooms-view.component.html',
+  styleUrl: './rooms-view.component.css'
 })
-export class ForumListComponent {
+export class RoomsViewComponent {
+  constructor(public dialog: MatDialog) {}
 
+  openCreateRoomDialog(): void {
+    this.dialog.open(CreateRoomDialogComponent);
+  }
 }
