98b49a2f4cb88562a587cfc77ca84be3f8055e64
Deskjobo <johan.bottger@gmail.com>
2024-09-24 16:11:48 -0500

    Feature Lists
    Lista de Favoritos y Ver más tarde utilizando los datos de db.json y enrutado al sidebarnav component

--- src/app/lists/pages/favorites/favorites.component.ts
+++ src/app/lists/pages/favorites/favorites.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { NgForOf } from "@angular/common";
+import { Component , signal } from '@angular/core';
+import { MatCardModule } from "@angular/material/card";
+import { MatTabsModule } from "@angular/material/tabs";
+import contentData from "../../../../../server/db.json";
+
+
+type CardContent = {
+  title: string;
+  description:string;
+  type: string;
+  feeling: string;
+  objective: string;
+}
 
 @Component({
   selector: 'app-favorites',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    NgForOf,
+    MatTabsModule
+  ],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.css'
 })
 export class FavoritesComponent {
+  cards = signal<CardContent[]>([]);
+
+  constructor() {
+    const data = contentData.content;
+    const cards: CardContent[] = data.map((item: any) => ({
+      title: item.title,
+      description: item.description,
+      type: item.type,
+      feeling: item.feeling,
+      objective: item.objective
+    }));
+    this.cards.set(cards);
+  }
 
 }
