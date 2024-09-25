8081f3c1b2e3fb3b80503bd11c096d6eca50a9b6
NLH18 <u20221b226@upc.edu.pe>
2024-09-25 17:04:07 -0500

    feat: fixed available content explorer page

---
+++ src/app/explorer/components/content-list/content-list.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input} from '@angular/core';
+import {ContentItemComponent} from "../content-item/content-item.component";
+import {Content} from "../../model/content.entity";
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+import {NgForOf} from "@angular/common";
+
+@Component({
+  selector: 'app-content-list',
+  standalone: true,
+  imports: [ContentItemComponent, MatGridList, MatGridTile, NgForOf],
+  templateUrl: './content-list.component.html',
+  styleUrl: './content-list.component.css'
+})
+export class ContentListComponent {
+  @Input() contents: Array<Content>=[];
+}
