966a7541c43701c98e6a4b0b6f84c0735cd97e24
Gabriel Lapa <u202216831@upc.edu.pe>
2024-09-25 18:08:25 -0500

    feat: add event entity

--- src/app/events/model/event.entity.ts
+++ src/app/events/model/event.entity.ts
@@ -1,2 +1,28 @@
+
 export class Event {
+  eventId: string;
+  contentId: string;
+  title: string;
+  description: string;
+  date: string;
+  address: string;
+  creatorId: string;
+
+  constructor(
+    eventId: string,
+    contentId: string,
+    title: string,
+    description: string,
+    date: string,
+    address: string,
+    creatorId: string
+  ) {
+    this.eventId = eventId;
+    this.contentId = contentId;
+    this.title = title;
+    this.description = description;
+    this.date = date;
+    this.address = address;
+    this.creatorId = creatorId;
+  }
 }
