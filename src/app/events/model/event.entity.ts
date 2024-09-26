421b30baf76e91ae3e58d582d14d057b015097f1
Gabriel Lapa <u202216831@upc.edu.pe>
2024-09-25 19:19:04 -0500

    feat: update event entity

--- src/app/events/model/event.entity.ts
+++ src/app/events/model/event.entity.ts
@@ -8,13 +8,13 @@ export class Event {
   address: string;
   creatorId: number;
 
-  constructor(event:{eventId?: number, contentId?: number, title?: string, description?: string, date?: string, address?: string, creatorId?: number}) {
-    this.eventId = event.eventId || 0;
-    this.contentId = event.contentId || 0;
-    this.title = event.title || '';
-    this.description = event.description || '';
-    this.date = event.date || '';
-    this.address = event.address || '';
-    this.creatorId = event.creatorId || 0;
+  constructor() {
+    this.eventId = 0;
+    this.contentId = 0;
+    this.title = "";
+    this.description = "";
+    this.date = "";
+    this.address = "";
+    this.creatorId = 0;
   }
 }
