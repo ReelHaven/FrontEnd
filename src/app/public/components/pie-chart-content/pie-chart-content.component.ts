599b367a1429a38d8398afab8440abf4b2da0f33
PieroMiranda1 <pieroms25@gmail.com>
2024-09-26 02:37:04 -0500

    chore: added structure for pie chart

---
+++ src/app/public/components/pie-chart-content/pie-chart-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pie-chart-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './pie-chart-content.component.html',
+  styleUrl: './pie-chart-content.component.css'
+})
+export class PieChartContentComponent {
+
+}
