6c136e30ee258e99b269a1e94a4d0346f115eb1a
PieroMiranda1 <pieroms25@gmail.com>
2024-09-26 02:53:18 -0500

    chore: added pie chart to home view

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -2,13 +2,15 @@ import {Component, OnInit} from '@angular/core';
 import {SidebarNavigatorComponent} from "../../components/sidebar-navigator/sidebar-navigator.component";
 import {MoodService} from "../../../users/services/mood.service";
 import {RecommendationContentComponent} from "../../components/recommendation-content/recommendation-content.component";
+import {PieChartContentComponent} from "../../components/pie-chart-content/pie-chart-content.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     SidebarNavigatorComponent,
-    RecommendationContentComponent
+    RecommendationContentComponent,
+    PieChartContentComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
